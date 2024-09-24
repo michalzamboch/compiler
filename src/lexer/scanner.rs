154f401f9c1499400396a84142ea7e7abc8d9b75
Michal Žamboch <zamboch.michal@seznam.cz>
2024-09-24 16:14:26 +0200

    Refactoring.

--- src/lexer/scanner.rs
+++ src/lexer/scanner.rs
@@ -63,106 +63,67 @@ impl Scanner {
     }
 
     fn create_correct_token(&self, element: &str, state: &TempState) -> Option<Token> {
+        let literal = element.to_string();
+        let line = state.current_line;
+
         let token = match element {
-            "=" => Token::new(TokenType::Equal, element.to_string(), state.current_line),
-            "==" => Token::new(
-                TokenType::EqualEqual,
-                element.to_string(),
-                state.current_line,
-            ),
-            ">" => Token::new(TokenType::Greater, element.to_string(), state.current_line),
-            ">=" => Token::new(
-                TokenType::GreaterEqual,
-                element.to_string(),
-                state.current_line,
-            ),
-            "<=" => Token::new(
-                TokenType::LessEqual,
-                element.to_string(),
-                state.current_line,
-            ),
-            "<" => Token::new(TokenType::Less, element.to_string(), state.current_line),
-            "!=" => Token::new(
-                TokenType::BangEqual,
-                element.to_string(),
-                state.current_line,
-            ),
-            "!" => Token::new(TokenType::Bang, element.to_string(), state.current_line),
-            ";" => Token::new(
-                TokenType::Semicolon,
-                element.to_string(),
-                state.current_line,
-            ),
-            ")" => Token::new(
-                TokenType::RightParen,
-                element.to_string(),
-                state.current_line,
-            ),
-            "(" => Token::new(
-                TokenType::LeftParen,
-                element.to_string(),
-                state.current_line,
-            ),
-            "}" => Token::new(
-                TokenType::RightBrace,
-                element.to_string(),
-                state.current_line,
-            ),
-            "{" => Token::new(
-                TokenType::LeftBrace,
-                element.to_string(),
-                state.current_line,
-            ),
-            "]" => Token::new(
-                TokenType::RightSquareBracket,
-                element.to_string(),
-                state.current_line,
-            ),
-            "[" => Token::new(
-                TokenType::LeftSquareBracket,
-                element.into(),
-                state.current_line,
-            ),
-            "+" => Token::new(TokenType::Plus, element.to_string(), state.current_line),
-            "-" => Token::new(TokenType::Minus, element.to_string(), state.current_line),
-            "/" => Token::new(TokenType::Slash, element.to_string(), state.current_line),
-            "*" => Token::new(TokenType::Star, element.to_string(), state.current_line),
-            "." => Token::new(TokenType::Dot, element.to_string(), state.current_line),
-            "," => Token::new(TokenType::Comma, element.to_string(), state.current_line),
-            "||" => Token::new(TokenType::Or, element.to_string(), state.current_line),
-            "&&" => Token::new(TokenType::And, element.to_string(), state.current_line),
-            "true" => Token::new(TokenType::True, element.to_string(), state.current_line),
-            "false" => Token::new(TokenType::False, element.to_string(), state.current_line),
-            "int" => Token::new(TokenType::Int, element.to_string(), state.current_line),
-            "float" => Token::new(TokenType::Float, element.to_string(), state.current_line),
-            "string" => Token::new(TokenType::String, element.to_string(), state.current_line),
-            "bool" => Token::new(TokenType::Boolean, element.to_string(), state.current_line),
-            "if" => Token::new(TokenType::If, element.to_string(), state.current_line),
-            "else" => Token::new(TokenType::Else, element.to_string(), state.current_line),
-            "for" => Token::new(TokenType::For, element.to_string(), state.current_line),
-            "while" => Token::new(TokenType::While, element.to_string(), state.current_line),
-            "fun" => Token::new(TokenType::Fun, element.to_string(), state.current_line),
-            "return" => Token::new(TokenType::Return, element.to_string(), state.current_line),
-            "print" => Token::new(TokenType::Print, element.to_string(), state.current_line),
-            "nil" => Token::new(TokenType::Nil, element.to_string(), state.current_line),
-            _ => self.create_variable_token(element, state.current_line),
+            "=" => Token::new(TokenType::Equal, literal, line),
+            "==" => Token::new(TokenType::EqualEqual, literal, line),
+            ">" => Token::new(TokenType::Greater, literal, line),
+            ">=" => Token::new(TokenType::GreaterEqual, literal, line),
+            "<=" => Token::new(TokenType::LessEqual, literal, line),
+            "<" => Token::new(TokenType::Less, literal, line),
+            "!=" => Token::new(TokenType::BangEqual, literal, line),
+            "!" => Token::new(TokenType::Bang, literal, line),
+            ";" => Token::new(TokenType::Semicolon, literal, line),
+            ")" => Token::new(TokenType::RightParen, literal, line),
+            "(" => Token::new(TokenType::LeftParen, literal, line),
+            "}" => Token::new(TokenType::RightBrace, literal, line),
+            "{" => Token::new(TokenType::LeftBrace, literal, line),
+            "]" => Token::new(TokenType::RightSquareBracket, literal, line),
+            "[" => Token::new(TokenType::LeftSquareBracket, literal, line),
+            "+" => Token::new(TokenType::Plus, literal, line),
+            "-" => Token::new(TokenType::Minus, literal, line),
+            "/" => Token::new(TokenType::Slash, literal, line),
+            "*" => Token::new(TokenType::Star, literal, line),
+            "." => Token::new(TokenType::Dot, literal, line),
+            "," => Token::new(TokenType::Comma, literal, line),
+            "||" => Token::new(TokenType::Or, literal, line),
+            "&&" => Token::new(TokenType::And, literal, line),
+            "true" => Token::new(TokenType::True, literal, line),
+            "false" => Token::new(TokenType::False, literal, line),
+            "int" => Token::new(TokenType::Int, literal, line),
+            "float" => Token::new(TokenType::Float, literal, line),
+            "string" => Token::new(TokenType::String, literal, line),
+            "bool" => Token::new(TokenType::Boolean, literal, line),
+            "if" => Token::new(TokenType::If, literal, line),
+            "else" => Token::new(TokenType::Else, literal, line),
+            "for" => Token::new(TokenType::For, literal, line),
+            "while" => Token::new(TokenType::While, literal, line),
+            "fun" => Token::new(TokenType::Fun, literal, line),
+            "return" => Token::new(TokenType::Return, literal, line),
+            "print" => Token::new(TokenType::Print, literal, line),
+            "nil" => Token::new(TokenType::Nil, literal, line),
+            _ => self.create_variable_token(element, line),
         };
         Some(token)
     }
 
     fn create_variable_token(&self, element: &str, line: i32) -> Token {
+        let literal = element.to_string();
+
         if self.is_string_literal(element) {
-            Token::new(TokenType::Str, element.to_string(), line)
+            Token::new(TokenType::Str, literal, line)
         } else if self.is_number(element) {
             if element.contains(".") {
-                Token::new(TokenType::Real, element.to_string(), line)
+                Token::new(TokenType::Real, literal, line)
             } else {
-                Token::new(TokenType::Interger, element.to_string(), line)
+                Token::new(TokenType::Interger, literal, line)
             }
         } else if self.is_idetifier(element) {
-            Token::new(TokenType::Identifier, element.to_string(), line)
+            Token::new(TokenType::Identifier, literal, line)
         } else {
-            Token::new(TokenType::Unknown, element.to_string(), line)
+            Token::new(TokenType::Unknown, literal, line)
         }
     }
 
